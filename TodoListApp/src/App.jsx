14d99c2ee435dfdb9671812cd3fdfbce2188dcd7
sargamsubedi <saragamsubedi998@gmail.com>
2026-03-28 18:07:25 +0545

    initial commit

---
+++ TodoListApp/src/App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./todo"
+
+function App() {
+  return (
+    <>
+      <h1>todo app</h1>
+
+      <Todo />
+    </>
+  )
+}
+
+export default App
---
+++ TodoListApp/src/todo.jsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+
+function Todo()
+{
+    const [tasks,setTasks] =useState([]);
+    const [newTask,setNewTask] =useState("");
+    const idref=useRef(0);
+
+    function addTask()
+    {   if(newTask==="") return; // dont add task if nothing in input
+        setTasks([...tasks,{"id":idref.current, "text":newTask}]);
+        idref.current=idref.current+1;
+
+        setNewTask(""); // empty input value
+
+    }
+
+    function removeTask(Id)
+    {
+        let tempTaskArr = tasks.filter((task)=>{
+            if(Id!==task.id) return task;
+        })
+        setTasks(tempTaskArr);
+        
+        
+    }
+
+    return(
+            <>
+                <input type="text"  value={newTask} placeholder="Add new task" onChange={(e)=>{setNewTask(e.target.value)}} />
+                <button onClick={addTask}>Add</button>
+
+                {
+                    tasks.map((task,index)=>(
+                        <div key={task.id}>
+                            <p>{index+1}: {task.text}</p>
+                            <button onClick={()=>{removeTask(task.id)
+                            }
+                            }>Delete</button>
+                        </div>
+                    ))
+                }
+            </>
+
+
+    )
+}
+
+export default Todo;
