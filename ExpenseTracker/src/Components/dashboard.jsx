45c0dcd9fbee7717e3216f7f2657b118bb746f44
sargamsubedi <saragamsubedi998@gmail.com>
2026-03-29 18:04:35 +0545

    add: no expense add case in dashboard

--- ExpenseTracker/src/Components/dashboard.jsx
+++ ExpenseTracker/src/Components/dashboard.jsx
@@ -5,40 +5,45 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 function Dashboard() {
     const { isLoggedIn } = useContext(AuthContext);
-    const {expenses, setExpenses} =useContext(ExpenseContext);
-    const navigate =useNavigate();
+    const { expenses, setExpenses } = useContext(ExpenseContext);
+    const navigate = useNavigate();
 
-    function removeitem(Id)
-    {
-        const tempExpenses= expenses.filter((ep)=>( ep.id!==Id
-            )
+    function removeitem(Id) {
+        const tempExpenses = expenses.filter((ep) => (ep.id !== Id
         )
-    setExpenses(tempExpenses);
-        
+        )
+        setExpenses(tempExpenses);
+
     }
 
 
-    if (!isLoggedIn) return <Navigate to="/"/>
+    if (!isLoggedIn) return <Navigate to="/" />
 
-    if(expenses.length === 0) return(<h2>No items added yet..</h2>)
+    if (expenses.length === 0) return (
+        <>
+            <h2>No items added yet..</h2>
+            <button onClick={() => { navigate("/addexpense") }}>Add Item</button>
+
+        </>
+    )
     return (
         <>
-        <h1>this is dashboard</h1>
-        <button onClick={()=>{navigate("/addexpense")}}>Add Item</button>
+            <h1>this is dashboard</h1>
+            <button onClick={() => { navigate("/addexpense") }}>Add Item</button>
 
-        {
-            expenses.map((ep,index)=>(
-                <div key={ep.id}>
-                    <p>item {index+1}: </p>
-                    <p>title: {ep.title}</p>
-                    <p>price: {ep.amount}</p>
-                    <p>category: {ep.category}</p>
-                    <button onClick={()=>{
-                        removeitem(ep.id)
-                    }}>Delete Item</button>
-                </div>
-            ))
-        }
+            {
+                expenses.map((ep, index) => (
+                    <div key={ep.id}>
+                        <p>item {index + 1}: </p>
+                        <p>title: {ep.title}</p>
+                        <p>price: {ep.amount}</p>
+                        <p>category: {ep.category}</p>
+                        <button onClick={() => {
+                            removeitem(ep.id)
+                        }}>Delete Item</button>
+                    </div>
+                ))
+            }
         </>
     )
 
