3e980af72b917dde8ebe5ccca774ea88c4e533f4
sargamsubedi <saragamsubedi998@gmail.com>
2026-04-01 23:30:35 +0545

    feat: handle loading more user friendly

--- Search/src/Display.jsx
+++ Search/src/Display.jsx
@@ -7,7 +7,7 @@ import useDebounce from "./CustomHooks/useDebounce";
     const [input,setInput]= useState("");
     const [page, setPage] =useState(1);
     const debouncedvalue = useDebounce(input);
-    const { data, loading, error, moreDataAvailable } = useFetch(debouncedvalue, 50 ,page)
+    const { data, loading, error, moreDataAvailable } = useFetch(debouncedvalue, 5 ,page)
 
 
     useEffect(()=>{
@@ -19,17 +19,20 @@ import useDebounce from "./CustomHooks/useDebounce";
         return <h1>An error occurred, please refresh the page</h1>
     }
 
-    if(loading)
+    if(loading && page===1)
     {
         return <h1>The data is fetching.. Please Wait</h1>
     }
 
 return(
     <>
-        <input type="text" placeholder="Search here" onChange={(e)=>setInput(e.target.value)}/>
+        <input type="text" placeholder="Search here" value={input} onChange={(e)=>setInput(e.target.value)}/>
         <button onClick={()=>setPage(prev=>prev+1)} disabled={loading || !moreDataAvailable} >Load More</button>
 
-        {
+    {
+        loading && page!==1 && <p>more data loading...</p>
+    }
+        { 
             data.map((dat)=>(
                 <div key={dat.id}>
                     {dat.id}: {dat.title}
