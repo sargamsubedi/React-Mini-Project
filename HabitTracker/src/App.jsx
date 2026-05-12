d9b05f7b18075ce5adfc10103ab1bd9a8dc74643
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-12 22:13:32 +0545

    setup new project : HabitTracker

---
+++ HabitTracker/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>Habit Tracker</h1>
+      <p>This project is the implementation of Zustand i recently learned</p>
+    </>
+  )
+}
+
+export default App
