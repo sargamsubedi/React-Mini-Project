c4f075f0a692f4594b11f782f483dc8e4363decf
sargamsubedi <saragamsubedi998@gmail.com>
2026-05-14 23:32:56 +0545

    fix logic in calculateStreak

--- HabitTracker/src/utils/calculateStreak.js
+++ HabitTracker/src/utils/calculateStreak.js
@@ -1,9 +1,11 @@
+import { getTodayDate } from "./getTodayDate";
 
 export function calculateStreak(history)
 {
     let steak=0;
-
+    const today= getTodayDate();
     for (const date in history) {
+        if(date===today) continue; // skip today (because day isnt complete so streak shouldn't be reset)
         steak= Boolean(history[date])? steak+1 : 0;
     }
 
